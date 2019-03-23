21b600a21bab34ec4667626bc65d81b35f3b1dd4
netaro-N <takebbb1+github@gmail.com>
2019-03-23 07:57:11 +0000

    fisrt scene

---
+++ js/cubeRotation.js
@@ -0,0 +1,7 @@
+const scene = new THREE.Scene(); //シーンを作成
+const camera = new THREE.perspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
