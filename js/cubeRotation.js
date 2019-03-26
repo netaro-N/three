82a9125ae529f214a5a2b9e2480adb1dad113eaa
netaro-N <takebbb1+github@gmail.com>
2019-03-26 07:27:32 +0000

    first blender

--- js/cubeRotation.js
+++ js/cubeRotation.js
@@ -6,16 +6,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+/*
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+*/
+
+// 環境光を追加
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+scene.add(ambientLight);
+
+// Collada 形式のモデルデータを読み込む
+const loader = new THREE.ColladaLoader();
+// dae ファイルのパスを指定
+loader.load('models/dae/circle.dae', (collada) => {
+    const model = collada.scene;
+    scene.add(model); // 読み込み後に3D空間に追加
+});
 
 function animate() {
   requestAnimationFrame( animate );
 
+  /*
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+*/
 
   renderer.render( scene, camera );
 }
