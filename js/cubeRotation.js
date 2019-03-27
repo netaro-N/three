ca6f3f67e532225e5efb3b0e581fab1e3469bc0f
netaro-N <takebbb1+github@gmail.com>
2019-03-27 13:15:48 +0000

    commit

--- js/cubeRotation.js
+++ js/cubeRotation.js
@@ -2,6 +2,7 @@ const scene = new THREE.Scene(); //シーンを作成
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
 
+const controls = new THREE.OrbitControls(camera);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
